302319a0f162221b20b7cbe56d604f6c619f1fe4
Saloua0 <153821629+Saloua0@users.noreply.github.com>
2024-01-06 11:53:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                lat: 35.61872,
-                lng: -5.27525,
+                lat: 35.618681,
+                lng: -5.275370,
             },
         },
     ];
